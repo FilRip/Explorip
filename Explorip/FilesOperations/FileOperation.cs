947713fff1a0f1c6ecd5df6426d6c0d54b1b8253
FilRip <filrip@yahoo.fr>
2023-03-08 18:17:49 +0100

    Add test library to hook shell copy operations

--- Explorip/FilesOperations/FileOperation.cs
+++ Explorip/FilesOperations/FileOperation.cs
@@ -13,6 +13,7 @@ namespace Explorip.FilesOperations
     //        https://stackoverflow.com/questions/73185376/transfer-files-remotelly-via-delayed-renderingcf-hdrop
     //        https://stackoverflow.com/questions/74151301/c-hook-windows-explorer-paste-event
     //        https://github.com/mity/old-new-win32api#drag-and-drop
+    //        https://stackoverflow.com/questions/1585453/detect-windows-explorer-copy-operation
     public class FileOperation : IDisposable
     {
         private static readonly Guid CLSID_FileOperation = new("3ad05575-8857-4850-9277-11b85bdb8e09");
---
+++ ExploripShellExt/ExploripCopyHook.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ExploripShellExt
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("89E45E24-C007-418F-9AB8-576BE44D7EC6")]
+    [ComVisible(true)]
+    public class ExploripCopyHook : ICopyHookW
+    {
+        public uint CopyCallback(IntPtr hwnd, FILEOP fileOperation, uint flags, [MarshalAs(UnmanagedType.LPWStr)] string srcFile, uint srcAttribs, [MarshalAs(UnmanagedType.LPWStr)] string destFile, uint destAttribs)
+        {
+            NativeWindow owner = new();
+            owner.AssignHandle(hwnd);
+            try
+            {
+                DialogResult result = DialogResult.Yes;
+
+                /*if (Process.GetProcessesByName("Explorip")?.Length > 0)
+                {
+                    return (uint)DialogResult.No;
+                }
+                else
+                    return (uint)DialogResult.Yes;*/
+
+                // If the file name contains "Test" and it is being renamed...
+                if (srcFile.Contains("Test") && fileOperation == FILEOP.Rename)
+                {
+                    result = MessageBox.Show(owner,
+                        String.Format("Are you sure to rename the folder {0} as {1} ?",
+                        srcFile, destFile), "CSShellExtCopyHookHandler",
+                        MessageBoxButtons.YesNoCancel);
+                }
+
+                Debug.Assert(result == DialogResult.Yes || result == DialogResult.No ||
+                    result == DialogResult.Cancel);
+                return (uint)result;
+            }
+            finally
+            {
+                owner.ReleaseHandle();
+            }
+        }
+
+        #region Shell Extension Registration
+
+        [ComRegisterFunction()]
+        public static void Register(Type t)
+        {
+            try
+            {
+                ShellExtReg.RegisterShellExtCopyHookHandler(
+                    "ExploripShellExt", t.GUID);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); // Log the error
+                throw;  // Re-throw the exception
+            }
+        }
+
+        [ComUnregisterFunction()]
+        public static void Unregister(Type t)
+        {
+            try
+            {
+                ShellExtReg.UnregisterShellExtCopyHookHandler(
+                    "ExploripShellExt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); // Log the error
+                throw;  // Re-throw the exception
+            }
+        }
+
+        #endregion
+    }
+}
