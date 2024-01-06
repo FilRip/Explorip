5e224b5bcd68cb4bf30209a7644395f1b7cc7cfa
FilRip <filrip@gmail.com>
2024-01-06 01:48:46 +0100

    Convert csproj to new format Sdk

--- HookFileOperation/ExploripSharedCopy/Properties/AssemblyInfo.cs
+++ HookFileOperation/ExploripSharedCopy/Properties/AssemblyInfo.cs
@@ -4,18 +4,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// Les informations générales relatives à un assembly dépendent de
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-[assembly: AssemblyTitle("ExploripSharedCopy")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExploripSharedCopy")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
 // aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
@@ -39,17 +27,3 @@ using System.Windows;
                                       //(utilisé si une ressource est introuvable dans la page,
                                       // dans l'application ou dans l'un des dictionnaires de ressources spécifiques à un thème)
 )]
-
-
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
-// en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
