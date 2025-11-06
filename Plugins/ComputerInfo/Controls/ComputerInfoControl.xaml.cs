fd479f47093d31c1ee61f117423029be0d55b59f
FilRip <filrip@gmail.com>
2025-11-06 17:30:10 +0100

    Wrong sub folder for project ComputerInfo

---
+++ Plugins/ComputerInfo/Controls/ComputerInfoControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using ComputerInfo.ViewModels;
+
+namespace ComputerInfo.Controls;
+
+public partial class ComputerInfoControl : UserControl
+{
+    public ComputerInfoControl()
+    {
+        InitializeComponent();
+    }
+
+    public ComputerInfoViewModel MyDataContext
+    {
+        get { return (ComputerInfoViewModel)DataContext; }
+    }
+
+    private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+    {
+        MyDataContext.Dispose();
+    }
+}
