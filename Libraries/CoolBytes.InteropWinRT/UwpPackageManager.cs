5bb60167038d04697cf836922ab9cc27a8ae855b
FilRip <filrip@gmail.com>
2026-01-13 19:12:04 +0100

    Optimize search string resource of UWP application

--- Libraries/CoolBytes.InteropWinRT/UwpPackageManager.cs
+++ Libraries/CoolBytes.InteropWinRT/UwpPackageManager.cs
@@ -13,6 +13,8 @@ namespace CoolBytes.InteropWinRT;
 
 public static class UwpPackageManager
 {
+    private const string DEFAULT_PRI_FILES = "resources.pri";
+
     public static string? PathOfUwp(string appUserModelId)
     {
         PackageManager packageManager = new();
@@ -24,43 +26,50 @@ public static class UwpPackageManager
     public static string? StringOfUwp(string appUserModelId, string msResource)
     {
         string? path = PathOfUwp(appUserModelId);
-        msResource = msResource.Replace("ms-resource:///Resources/", "");
         if (!string.IsNullOrWhiteSpace(path) && Directory.Exists(path))
         {
-            if (File.Exists(Path.Combine(path, "resources.pri")))
+            msResource = msResource.Replace("ms-resource:///Resources/", "");
+            if (File.Exists(Path.Combine(path, DEFAULT_PRI_FILES)))
             {
-                using FileStream fs = new(Path.Combine(path, "resources.pri"), FileMode.Open, FileAccess.Read);
-                PriFile priFile = PriFile.Parse(fs);
-                if (priFile.PriDescriptorSection.PrimaryResourceMapSection != null)
+                string? result = GetStringFromPri(Path.Combine(path, DEFAULT_PRI_FILES), msResource);
+                if (result != null)
+                    return result;
+            }
+            foreach (string otherPriFile in Directory.GetFiles(path, "*.pri").Where(f => Path.GetFileName(f).ToLower() != DEFAULT_PRI_FILES))
+            {
+                string? result = GetStringFromPri(otherPriFile, msResource);
+                if (result != null)
+                    return result;
+            }
+        }
+        return null;
+    }
+
+    public static string? GetStringFromPri(string path, string msResource)
+    {
+        using FileStream fs = new(path, FileMode.Open, FileAccess.Read);
+        PriFile priFile = PriFile.Parse(fs);
+        if (priFile.PriDescriptorSection.PrimaryResourceMapSection != null)
+        {
+            ResourceMapSection resourceMapSection = priFile.GetSectionByRef(priFile.PriDescriptorSection.PrimaryResourceMapSection.Value);
+            HierarchicalSchemaSection schemaSection = priFile.GetSectionByRef(resourceMapSection.SchemaSection);
+            ResourceMapItem? item = schemaSection.Items.FirstOrDefault(section => section.Name == msResource);
+            if (item != null &&
+                resourceMapSection.CandidateSets.TryGetValue(item.Index, out CandidateSet? candidateSet))
+            {
+                Candidate candidate = candidateSet.Candidates.FirstOrDefault(c => c.DataItem != null &&
+                                                                                  (c.Type == EResourceValueType.String || c.Type == EResourceValueType.AsciiString || c.Type == EResourceValueType.Utf8String));
+                if (candidate != null)
                 {
-                    ResourceMapSection resourceMapSection = priFile.GetSectionByRef(priFile.PriDescriptorSection.PrimaryResourceMapSection.Value);
-                    HierarchicalSchemaSection schemaSection = priFile.GetSectionByRef(resourceMapSection.SchemaSection);
-                    foreach (ResourceMapItem item in schemaSection.Items)
-                    {
-                        if (item.Name == msResource)
-                        {
-                            if (resourceMapSection.CandidateSets.TryGetValue(item.Index, out CandidateSet? candidateSet))
-                            {
-                                foreach (Candidate candidate in candidateSet.Candidates)
-                                {
-                                    if (candidate.Type == EResourceValueType.String || candidate.Type == EResourceValueType.AsciiString || candidate.Type == EResourceValueType.Utf8String)
-                                    {
-                                        if (candidate.DataItem == null)
-                                            continue;
-                                        ByteSpan data = priFile.GetDataItemByRef(candidate.DataItem.Value);
-                                        fs.Seek(data.Offset, SeekOrigin.Begin);
-                                        byte[] buffer = new byte[data.Length];
-                                        _ = fs.Read(buffer, 0, buffer.Length);
-                                        Encoding encoding = Encoding.ASCII;
-                                        if (candidate.Type == EResourceValueType.Utf8String)
-                                            encoding = Encoding.UTF8;
-                                        string result = encoding.GetString(buffer);
-                                        return result.TrimEnd((char)0);
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    ByteSpan data = priFile.GetDataItemByRef(candidate.DataItem!.Value);
+                    fs.Seek(data.Offset, SeekOrigin.Begin);
+                    byte[] buffer = new byte[data.Length];
+                    _ = fs.Read(buffer, 0, buffer.Length);
+                    Encoding encoding = Encoding.ASCII;
+                    if (candidate.Type == EResourceValueType.Utf8String)
+                        encoding = Encoding.UTF8;
+                    string result = encoding.GetString(buffer);
+                    return result.TrimEnd((char)0);
                 }
             }
         }
