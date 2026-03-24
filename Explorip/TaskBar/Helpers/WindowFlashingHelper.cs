78dd1d0e02ae8022e5aa7b933ba1fce5fbb4f66d
FilRip <filrip@gmail.com>
2026-03-24 15:59:08 +0100

    Support window flashing in task bar list

---
+++ Explorip/TaskBar/Helpers/WindowFlashingHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Explorip.TaskBar.Helpers;
+
+public static class WindowFlashingHelper
+{
+    public static readonly DependencyProperty IsFlashingProperty =
+        DependencyProperty.RegisterAttached(
+            nameof(IsFlashing),
+            typeof(bool),
+            typeof(WindowFlashingHelper),
+            new FrameworkPropertyMetadata(false));
+
+    public static void SetIsFlashing(DependencyObject element, bool value)
+    {
+        element.SetValue(IsFlashingProperty, value);
+    }
+
+    public static bool GetIsFlashing(DependencyObject element)
+    {
+        return (bool)element.GetValue(IsFlashingProperty);
+    }
+
+    public static bool IsFlashing { get; set; }
+}
